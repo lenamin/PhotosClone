da1be6ea95dbe2ab7313c3ce200ed77bad578dfc
lenamin <lenaminn@gmail.com>
2024-08-12 17:30:48 +0900

    [ADD] PhotoManager에 필요한 쿼리 구현

---
+++ PhotosClone/Utils/Constants/PreficateFormats.swift
@@ -0,0 +1,12 @@
+//
+//  PreficateFormats.swift
+//  PhotosClone
+//
+//  Created by Lena on 2024/8/10.
+//
+
+import Foundation
+
+struct PreficateFormats {
+    static let dateRange = "creationDate >= %@ AND creationDate < %@"
+}
